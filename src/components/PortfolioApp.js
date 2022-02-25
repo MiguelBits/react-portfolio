d89180710a9fb74137361d73d3646f5d6d45e6bd
mcruzvas <mcv.cooper@gmail.com>
2022-02-25 18:22:28 +0000

    new images

--- src/components/PortfolioApp.js
+++ src/components/PortfolioApp.js
@@ -4,10 +4,10 @@ import { Component } from 'react/cjs/react.production.min';
 
 class PortfolioApp extends Component {
     state = {
-        projects_collection: ["https://user-images.githubusercontent.com/15989933/153777731-e2be5a8a-18cb-46d7-889a-5375014f0c16.png",
-        "https://d5nunyagcicgy.cloudfront.net/external_assets/hero_examples/hair_beach_v391182663/original.jpeg",
-        "https://media.istockphoto.com/photos/colorful-background-of-pastel-powder-explosionrainbow-color-dust-on-picture-id1180542165?k=20&m=1180542165&s=612x612&w=0&h=43hlhk8qdGYP4V-u3AAxD3kPDRIzHjMNWpr-VdBQ2Js=",
-        "https://thumbs.dreamstime.com/b/d-image-abstract-tunnel-diagonal-black-white-stripes-optical-illusion-168695979.jpg"],
+        projects_collection: ["https://github.com/mcruzvas/react-portfolio/blob/master/public/image/NFT.jpg?raw=trueg",
+        "https://github.com/mcruzvas/react-portfolio/blob/master/public/image/Defi.jpg?raw=true",
+        "https://github.com/mcruzvas/react-portfolio/blob/master/public/image/Frontend.jpg?raw=true",
+        "https://github.com/mcruzvas/react-portfolio/blob/master/public/image/SocialMedia.jpg?raw=true"],
     
         projects_name: [
             "NFT", "Defi", "Frontend", "Social Media"
