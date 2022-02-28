fedb25a25ab08f398f77e67b47242797ca707e5c
mcruzvas <mcv.cooper@gmail.com>
2022-02-28 20:25:53 +0000

    dropdown v2 with stats+img

--- src/pages/nft_duel/pages/Battle.js
+++ src/pages/nft_duel/pages/Battle.js
@@ -5,7 +5,6 @@ import './../css/Battle.css';
 import "./../css/Duel.css"
 import Nav from "./../components/Nav"
 
-
 class Battle extends React.Component {
   state = {
     stakedPopulation: 0,
@@ -13,6 +12,12 @@ class Battle extends React.Component {
     openDropMenu: false,
     playerHero: 0,
     enemyHero: 0,
+    collection_tokenImg: [],
+    collection_tokenName: [],
+    collection_tokenClass: [],
+    collection_tokenElement: [],
+    collection_tokenAttack: [],
+    collection_tokenStars: [],
   }
   getStakedPopulation = () => {
     const { ethereum } = window;
@@ -26,7 +31,7 @@ class Battle extends React.Component {
           this.setState({stakedPopulation:result.length})
           for(var i = 0; i < result.length;i++){
             const previous_state_element = this.state.stakedIDs;
-            console.log(result[i])
+            //console.log(result[i])
             const updated_state_nft_element = previous_state_element.concat(parseInt(result[i]._hex))
             this.setState({stakedIDs:updated_state_nft_element})
           }
@@ -37,9 +42,63 @@ class Battle extends React.Component {
         console.log("Ethereum object does not exist");
       }
   }
+  getUriStakedTokens = async () => {
+    const { ethereum } = window;
+  
+    if (ethereum) {
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      const nftContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+      let data = await nftContract.getStakedPopulation();
+
+      for(let i = 0; i<=data.length;i++){
+        nftContract.uri(data[i]).then(urlValue => {
+          //console.log(urlValue)
+          fetch(urlValue)
+              .then(response => response.json())
+              .then((jsonData) => {
+                //update local storage listed by tokenId
+                //img
+                const previous_state_tokenImg= this.state.collection_tokenImg;
+                const updated_state_nft_tokenImg = previous_state_tokenImg.concat(jsonData.image)
+                this.setState({collection_tokenImg: updated_state_nft_tokenImg})
+                //name
+                const previous_state_tokenName = this.state.collection_tokenName;
+                const updated_state_nft_tokenName = previous_state_tokenName.concat(jsonData.name)
+                this.setState({collection_tokenName: updated_state_nft_tokenName})
+                //class
+                const previous_state_class = this.state.collection_tokenClass;
+                const updated_state_nft_class = previous_state_class.concat(jsonData.properties.class)
+                this.setState({collection_tokenClass: updated_state_nft_class})
+                //element
+                const previous_state_element = this.state.collection_tokenElement;
+                const updated_state_nft_element = previous_state_element.concat(jsonData.properties.element)
+                this.setState({collection_tokenElement: updated_state_nft_element})
+        })})
+        nftContract.getNFT_attack(data[i]).then(result => {
+          //attack
+          const previous_state_attack = this.state.collection_tokenAttack;
+          const updated_state_nft_attack = previous_state_attack.concat(parseInt(result._hex.toString()))
+          this.setState({collection_tokenAttack: updated_state_nft_attack})
+        })
+        nftContract.getNFT_stars(data[i]).then(result => {
+          //stars
+          const previous_state_stars = this.state.collection_tokenStars;
+          const updated_state_nft_stars = previous_state_stars.concat(parseInt(result._hex.toString()))
+          this.setState({collection_tokenStars: updated_state_nft_stars})
+        })
+      }
+    }
+    else {
+      console.log("Ethereum object does not exist");
+    }
+    
+  }
   componentDidMount = () => {
     this.getStakedPopulation();
     document.body.style.backgroundImage = 'url("https://wallpaperaccess.com/full/130220.jpg")';
+    this.getUriStakedTokens();
   }
 
   Duel = (tokenId,enemyId) => {
@@ -81,20 +140,33 @@ class Battle extends React.Component {
                   <p className="dd-header__title--bold">Your Duel Galaxy Heroes:</p>
                 </div>
                 <div className="dd-header__action">
-                  <p>{this.state.openDropMenu ? 'Close' : 'Open'}</p>
+                  <p>{this.state.openDropMenu ? 'Hide ❌' : 'Show ⭕️ '}</p>
                 </div>
               </div>
               {this.state.openDropMenu && (
                 <ul className="dd-list">
                   {this.state.stakedIDs.map((item,i) => (
                     <li className="dd-list-item" key={i}>
-                      <button type="button" onClick={() => this.handleOnClick(item)}>
-                        <span>{item}</span>
-                      </button>
+                      <link rel="preconnect" href="https://fonts.gstatic.com"/>
+                      <link href="https://fonts.googleapis.com/css2?family=Balsamiq+Sans:wght@700&display=swap" rel="stylesheet"/>
+                      <a onClick={() => this.handleOnClick(item)} className="neon-button">{item}</a>
+                      <img className='stakedHeroImg' alt={i} src={this.state.collection_tokenImg[i]}/>
+                      <ul className='stakedHeroStats'>
+                        <p>{this.state.collection_tokenName[i]}</p>
+                        <p>Stars: {this.state.collection_tokenStars[i]}</p>
+                        <p>Attack: {this.state.collection_tokenAttack[i]}</p>
+                        <p>Class: {this.state.collection_tokenClass[i]}</p>
+                        <p>Element: {this.state.collection_tokenElement[i]}</p>
+                      </ul>
                     </li>
                   ))}
                 </ul>
               )}
+            </div>
+
+
+            <div className='EnemyHero'>
+
             </div>
           
 
