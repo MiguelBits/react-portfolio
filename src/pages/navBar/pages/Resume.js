95989aa1d985cd5171241ff9b06b2536bdad09d2
mcruzvas <mcv.cooper@gmail.com>
2022-03-08 17:15:28 +0000

    starting resume page

--- src/pages/navBar/pages/Resume.js
+++ src/pages/navBar/pages/Resume.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import { Component } from 'react/cjs/react.production.min';
 import NavBar from "./../../../components/NavBar"
+import "./../css/Resume.css"
 
 class Resume extends Component {
 
     render(){
         return (
             <div className='resume-page'>
+                <div className='resume-main'>
+                        
+                </div>
                 <NavBar></NavBar>
-                    
             </div>
         )
     }
