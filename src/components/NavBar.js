3c0d704c74e985899baef43c1cec7427869d3152
mcruzvas <mcv.cooper@gmail.com>
2022-03-21 23:33:42 +0000

    added linkedin

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -26,8 +26,10 @@ class NavBar extends Component {
                 <li className="navBarMenu"><a href="/">Home</a></li>
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
                 <li className="socialLinks"><a href="mailto:mcruzvas@gmail.com" target="_blank" className="fa fa-envelope"></a></li>
+                <li className="socialLinks"><a href="https://www.linkedin.com/in/miguel-de-vasconcelos-6b6a27167/" target="_blank" className="fa fa-linkedin"></a></li>
                 <li className="socialLinks"><a href="https://github.com/mcruzvas" target="_blank" className="fa fa-github"></a></li>
                 <li className="socialLinks"><a href="https://twitter.com/0xDealer_bits" target="_blank" className="fa fa-twitter"></a></li>
+
             </ul> 
         )
     }
